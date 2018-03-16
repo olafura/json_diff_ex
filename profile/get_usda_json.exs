4f39016803f893961f1aaa68269e2e8218a7e63d
Jordan Day <jordand@companykitchen.com>
2018-03-16 09:47:55 -0500

    Update USDA url to https since http endpoint returns 301 now.

--- profile/get_usda_json.exs
+++ profile/get_usda_json.exs
@@ -1,5 +1,5 @@
 HTTPoison.start
-case HTTPoison.get("http://www.usda.gov/data.json") do
+case HTTPoison.get("https://www.usda.gov/data.json") do
   {:ok, %HTTPoison.Response{body: body, status_code: 200}} -> File.write!("profile/usda.json", body)
 end
 case HTTPoison.get("https://edg.epa.gov/data.json") do
