5ba8e49378911478806e788f0b4b88ed45011907
Olafur Arason <olafura@olafura.com>
2018-11-22 08:31:22 -0500

    Use node instead of nodejs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,14 @@ ExUnit.start(exclude: [:failing])
 
 defmodule JsHelp do
   def diff(s1, s2) do
-    case System.cmd("nodejs", ["js/test_helper.js", s1, s2]) do
+    case System.cmd("node", ["js/test_helper.js", s1, s2]) do
+      {result, 0} -> Poison.decode!(result)
+      {error, 1} -> error
+    end
+  end
+
+  def patch(s1, s2) do
+    case System.cmd("node", ["js/patch_test_helper.js", s1, s2]) do
       {result, 0} -> Poison.decode!(result)
       {error, 1} -> error
     end
